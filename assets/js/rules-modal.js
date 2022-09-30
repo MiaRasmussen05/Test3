19b99e83b487c8180f643c2b36b137c3c824b4e1
MiaRasmussen05 <rasmussenm376@gmail.com>
2022-09-30 21:50:27 +0000

    Add new tester3

--- assets/js/rules-modal.js
+++ assets/js/rules-modal.js
@@ -1,29 +1,39 @@
 const rulesBtn = document.querySelector('#rules-btn');
 const rulesModal = document.querySelector('.rules-modal');
 const closeRulesBtn = document.querySelector('#close-rules');
-
-function openRules(){
-    rulesModal.style.display = "block";
-};
-function closeRules(){
-    rulesModal.style.display = "none";
-};
-
-rulesBtn.addEventListener("click", openRules);
-closeRulesBtn.addEventListener("click", closeRules);
-
-/* Open the play modal */
-
 const playBtn = document.querySelector('#play-btn');
 const playModal = document.querySelector('.play-modal');
 const closePlayBtn = document.querySelector('#close-play');
 
+function openRules(){
+    rulesModal.style.display = "block";
+};
+
 function openPlay(){
     playModal.style.display = "block";
 };
+
+/* Open the play modal */
+
+function closeRules(){
+    rulesModal.style.display = "none";
+};
+
 function closePlay(){
     playModal.style.display = "none";
 };
 
+rulesBtn.addEventListener("click", openRules);
+closeRulesBtn.addEventListener("click", closeRules);
 playBtn.addEventListener("click", openPlay);
 closePlayBtn.addEventListener("click", closePlay);
+
+window.onclick = function(event) {
+    if (event.target == rules-modal) {
+        rulesModal.style.display = "none";
+    }
+  
+      if (event.target == play-modal) {
+          playModal.style.display = "none";
+      }
+  }
