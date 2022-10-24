44bf3eb1718f084209011fe97e1a2241076f9fca
MiaRasmussen05 <rasmussenm376@gmail.com>
2022-10-24 22:20:05 +0000

    Add tester for animation

---
+++ assets/js/alert.js
@@ -0,0 +1,13 @@
+const fullName = document.querySelector('fuName');
+const emailAdd = document.querySelector('email');
+const clickedRadio = document.querySelector('radio');
+const textArea = document.querySelector('teArea');
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    if (fullName !== '' || emailAdd !== '' || clickedRadio !== '' || textArea !== '') {
+
+        i.preventDefault();
+        i.returnValue = '';
+    };
+});
