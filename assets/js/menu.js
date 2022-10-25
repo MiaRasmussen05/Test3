712060843bb1c7ca602b6be8bf7ed5a846d9992e
MiaRasmussen05 <rasmussenm376@gmail.com>
2022-10-25 23:37:22 +0000

    Add tester for settings

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -5,3 +5,11 @@ function openToggleMenu() {
 function closeToggleMenu() {
     document.getElementById('toggleMenu').style.display = 'none';
 };
+
+function openSettings() {
+    document.getElementById('settings').style.display = "block";
+};
+
+function closeSettings() {
+    document.getElementById('settings').style.display = "none";
+};
