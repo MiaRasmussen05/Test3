19ae3f7a8e215f12720dc2723cbf2749e96c714d
MiaRasmussen05 <rasmussenm376@gmail.com>
2022-10-30 02:10:24 +0000

    Add tester2 for pop up welcome

--- assets/js/welcome.js
+++ assets/js/welcome.js
@@ -1,7 +1,10 @@
+/**
+ * Welcome message shows up when home page is loaded and dissapears
+ * there after
+ */
 let welcomeMessage = document.getElementById('welcome');
 
 window.addEventListener('DOMContentLoaded', (event) => {
-
     welcomeMessage.textContent += " to Snake Snack";
     welcomeMessage.style.width = "95%";
     welcomeMessage.style.display = "block";
@@ -16,31 +19,39 @@ window.addEventListener('DOMContentLoaded', (event) => {
     welcomeMessage.style.textShadow = "0px 0px 5px #000";
     welcomeMessage.style.padding = "0 0.5%";
     welcomeMessage.style.marginLeft = "2%";
-    welcomeMessage.style.transition = "all 2s";
-
+    welcomeMessage.style.transition = "all 2s ease-in";
 });
 
-    function welcomeLeave() {
+function welcomeLeave() {
+    message.style.display = "none"
+};
 
-        welcomeMessage.style.display = "none"
-    
+setTimeout("welcomeLeave()", 3000);
+
+function startMessage() {
+    welcomeMessage.style.display = "block"
+    welcomeMessage.innerHTML = "Let's get started!";
+};
+
+setTimeout("startMessage()", 3000);
+
+function startLeave() {
+    welcomeMessage.style.transition = "all 2s ease-out";
+    welcomeMessage.style.display = "none"
+};
+
+setTimeout("startLeave()", 5000);
+
+const firstTime = localStorage.getItem('visited');
+
+function time() {
+    if (firstTime == null) {
+        welcomeLeave();
+        startMessage();
+        startLeave();
+        sessionStorage.setItem("visited", 1);
+    } else {
+        welcomeLeave();
+        startLeave();
     };
-    
-    setTimeout("welcomeLeave()", 3000);
-
-    function startMessage() {
-
-        welcomeMessage.style.display = "block"
-        welcomeMessage.innerHTML = "Let's get started!";
-
-    };
-
-    setTimeout("startMessage()", 3000);
-
-    function startLeave() {
-
-        welcomeMessage.style.display = "none"
-
-    };
-
-    setTimeout("startLeave()", 5000);
+};
