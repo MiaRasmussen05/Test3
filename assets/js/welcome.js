0d98931a3490880b017821ea3985557d1007ffe3
MiaRasmussen05 <rasmussenm376@gmail.com>
2022-10-30 00:13:39 +0000

    Add tester for pop up welcome

---
+++ assets/js/welcome.js
@@ -0,0 +1,46 @@
+let welcomeMessage = document.getElementById('welcome');
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+    welcomeMessage.textContent += " to Snake Snack";
+    welcomeMessage.style.width = "95%";
+    welcomeMessage.style.display = "block";
+    welcomeMessage.style.position = "absolute";
+    welcomeMessage.style.backgroundColor = "#2B2B2C";
+    welcomeMessage.style.borderRadius = "10px";
+    welcomeMessage.style.borderBottomRightRadius = "100px";
+    welcomeMessage.style.borderTopLeftRadius = "100px";
+    welcomeMessage.style.boxShadow = "0 0 10px #000";
+    welcomeMessage.style.textAlign = "center";
+    welcomeMessage.style.color = "#DFDEE0";
+    welcomeMessage.style.textShadow = "0px 0px 5px #000";
+    welcomeMessage.style.padding = "0 0.5%";
+    welcomeMessage.style.marginLeft = "2%";
+    welcomeMessage.style.transition = "all 1s";
+
+});
+
+    function welcomeLeave() {
+
+        welcomeMessage.style.display = "none"
+    
+    };
+    
+    setTimeout("welcomeLeave()", 5000);
+
+    function startMessage() {
+
+        welcomeMessage.style.display = "block"
+        welcomeMessage.innerHTML = "Let's get started!";
+
+    };
+
+    setTimeout("startMessage()", 7000);
+
+    function startLeave() {
+
+        welcomeMessage.style.display = "none"
+
+    };
+
+    setTimeout("startLeave()", 11000);
